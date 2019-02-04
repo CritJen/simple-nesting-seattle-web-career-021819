2c2ff872e61e4d62a0f57ec2b69357ce26a86e5c
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-04 00:41:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,6 +124,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages][2] = "Assembly"
+		 programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+
+		 return programmer_hash[:dennis_ritchie][:languages]
 
 end
