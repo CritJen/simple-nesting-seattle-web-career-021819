b05589aef54d93864e9d4f4bb9dc0af80d817044
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-04 00:40:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,9 +104,6 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
 		 alans_new_info = "GUI"
-		 programmer_hash[:alan_kay][:known_for] = "GUI"
-
-		 return programmer_hash[:alan_kay][:known_for]
 
 
 end
@@ -127,5 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages][2] = "Assembly"
 
 end
